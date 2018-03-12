e9327de7cfb1ef76989b2a35142e4f0a64286550
Jannik Buhr <jannik.m.buhr@gmail.com>
2018-03-12 16:02:24 +0100

    Revert "adding ui elements for fits"
    This reverts commit 09fd028df9e75cac1d64eed3fa126f96484c1eed.

--- runShinyFlow.R
+++ runShinyFlow.R
@@ -1,5 +1,5 @@
 # If you don't have the following packages installed, do so with 'install.packages(...)'
-# shiny, shinydashboard, tidyverse, DT, minpack.lm, broom, modelr
+# shiny, shinydashboard, tidyverse, DT
 #
 # then run these two lines
 
