09fd028df9e75cac1d64eed3fa126f96484c1eed
Jannik Buhr <jannik.m.buhr@gmail.com>
2018-03-12 15:28:25 +0100

    adding ui elements for fits

--- app.R
+++ app.R
@@ -22,7 +22,11 @@ ui <- dashboardPage(
 
         # Inputs
         menuItem("Data", icon = icon("flask"), startExpanded = T,
-                 fileInput(inputId = "file", label = "Raw input data", accept = c("text/csv", ".csv"))
+                 fileInput(inputId = "file", label = "Raw input data",
+                           accept = c(
+                               "text/csv",
+                               "text/comma-separated-values,text/plain",
+                               ".csv"))
         ),
 
         menuItem("Options", icon = icon("cogs"), startExpanded = T,
@@ -37,9 +41,7 @@ ui <- dashboardPage(
                  textInput(inputId = "experiment", label = "Name of your experiment",
                            value = "Stopped Flow: H450-1"),
                  textInput(inputId = "run", label = "Run or numbering",
-                           value = "run: 1"),
-                 radioButtons(inputId = "phases", label = "type of exponential decay for the fit",
-                              choices = list("1 phase" = 1, "2 phase" = 2, "3 phase" = 3))
+                           value = "run: 1")
         )
 
     ),
@@ -79,10 +81,68 @@ ui <- dashboardPage(
                              plotOutput(outputId = "processed_plot")
                          )
                      )
+            ),
+            tabPanel("Fits",
+                     fluidRow(
+                         h1("1 Phase exponential dacay"),
+                         box(title = "Starting Settings",
+                             numericInput("1Y0", "Y0", value = 9.48),
+                             numericInput("1Plateau", "Plateau", value = 8.61),
+                             numericInput("1KFast", "KFast", value = 40),
+                             actionButton("1_go", "Go!")
+                         ),
+                         plotOutput(outputId = "1p_plot"),
+                         plotOutput(outputId = "1p_res"),
+                         box(title = "Fit",
+                             textOutput(outputId = "1p_text"),
+                             textOutput(outputId = "1p_gof")
+                         )
+                     ),
+                     fluidRow(
+                         h1("2 Phase exponential dacay"),
+                         box(title = "Starting Settings",
+                             numericInput("2Y0", "Y0", value = 9.48),
+                             numericInput("2Plateau", "Plateau", value = 8.61),
+                             numericInput("2KFast", "KFast", value = 40),
+                             numericInput("2KSlow", "KSlow", value = 5),
+                             numericInput("2PercentFast", "PercentFast", value = 90),
+                             actionButton("2_go", "Go!")
+                         ),
+                         plotOutput(outputId = "2p_plot"),
+                         plotOutput(outputId = "2p_res"),
+                         box(
+                             textOutput(outputId = "2p_text"),
+                             textOutput(outputId = "2p_gof")
+                         )
+                     ),
+                     fluidRow(
+                         h1("3 Phase exponential dacay"),
+                         box(title = "Starting Settings",
+                             numericInput("3Y0", "Y0", value = 9.48),
+                             numericInput("3Plateau", "Plateau", value = 8.61),
+                             numericInput("3KFast", "KFast", value = 40),
+                             numericInput("3Kmedium", "Kmedium", value = 10),
+                             numericInput("3KSlow", "KSlow", value = 5),
+                             numericInput("3PercentFast", "PercentFast", value = 90),
+                             numericInput("3PercentSlow", "PercentSlow", value = 5),
+                             actionButton("3_go", "Go!")
+                         ),
+                         plotOutput(outputId = "3p_plot"),
+                         plotOutput(outputId = "1p_res"),
+                         box(
+                             textOutput(outputId = "3p_text"),
+                             textOutput(outputId = "3p_gof")
+                         )
+                     )
             )
         )
     )
 )
+
+
+
+
+
 
 
 
@@ -123,12 +183,13 @@ server <- function(input, output) {
         inFile <- input$file
 
         # Check if a file is selected
-        req(inFile)
+        if (is.null(inFile))
+            return(NULL)
 
         df <- read_csv(inFile$datapath, skip = 30)
         df <- filter(df, !is.na(Time))
         df <- df %>% rename(fluorescence = Wavelength, time = Time)
-        df
+        return(df)
     })
 
     # Clean Raw data
@@ -162,7 +223,7 @@ server <- function(input, output) {
             ) %>%
             filter(!is.na(fluorescence)) %>%
             select(time, fluorescence)
-        df
+        return(df)
     })
 
     # Display Raw Data
@@ -180,7 +241,7 @@ server <- function(input, output) {
     # Download Processed data
     output$download <- downloadHandler(
         filename = function() {
-            paste(substr(input$file, 1, nchar(input$file)-4), "_processed", ".csv", sep = "")
+            paste(substr(input$file$name, 1, nchar(input$file)-4), "_processed", ".csv", sep = "")
         },
         content = function(con){
             write_csv(final(), con)
@@ -191,24 +252,63 @@ server <- function(input, output) {
     # Output plots
     # raw
     output$raw_plot <- renderPlot({
-        # s = input$raw_rows_selected
-        # new <- rawdata()[s,]
-        # p <- rawdata() %>% plt()
-        # p + geom_point(data = new, color = "red")
-        plot(data = rawdata(), fluorescence ~ time)
+        s = input$raw_rows_selected
+        new <- rawdata()[s,]
+        p <- rawdata() %>% plt()
+        p + geom_point(data = new, color = "red")
     })
+
     # processed
     output$processed_plot <- renderPlot({
         s = input$data_rows_selected
-        new <- rawdata()[s,]
+        new <- final()[s,]
         p <- final() %>% plt()
         p + geom_point(data = new, color = "red")
     })
 
-    # Fitted
-    output$fitted <- renderPlot({})
 
+    # Fit and make plots
+    # model1 <- eventReactive(input$`1_go`, {
+    #     starting1 <- list(Y = input$Yo, PLateau = input$Plateau, KFast = input$KFast)
+    #     model1 <- fit_decay(data(), 1)
+    #     return(model1)
+    # })
+    #
+    # data_model1 <- reactive({
+    #     df <- extract_model(data(), model1())
+    # })
+    #
+    #
+    # td1 <- reactive({
+    #     tidy(model1())
+    # })
+    #
+    # values1 <- reactiveValues()
+    #
+    # values1$y_start <- td1()$estimate[which(td1()$term == "Y0")]
+    # values1$y_end <- td1()$estimate[which(td1()$term == "Plateau")]
+    # values1$k_fast <- td1()$estimate[which(td1()$term == "KFast")]
+    # values1$k_med <- td1()$estimate[which(td1()$term == "Kmedium")]
+    # values1$k_slow <- td1()$estimate[which(td1()$term == "KSlow")]
+    # values1$p_slow <- td1()$estimate[which(td1()$term == "PercentSlow")]
+    # values1$p_fast <- td1()$estimate[which(td1()$term == "PercentFast")]
+    # values1$t_half_fast <- log(2) / values1$k_fast
+    # values1$t_half_slow <- log(2) / values1$k_slow
+    # values1$sigma <- glance(model1())$sigma
+    #
+    #
 
+    #
+    #     # Send to fitted plots output
+    #     output$`1p_plot` <- renderPlot({
+    #         p1()
+    #     })
+    #     output$`2p_plot` <- renderPlot({
+    #         p2()
+    #     })
+    #     output$`2p_plot` <- renderPlot({
+    #         p3()
+    #     })
 
 
 }
