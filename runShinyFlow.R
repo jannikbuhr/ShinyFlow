3e3059916deb05b41326b6f3143c19d01eff22fd
Jannik Buhr <jannik.m.buhr@gmail.com>
2018-03-13 10:24:27 +0100

    update the run-script

--- runShinyFlow.R
+++ runShinyFlow.R
@@ -1,7 +1,9 @@
-# If you don't have the following packages installed, do so with 'install.packages(...)'
+# If you don't have the following packages installed, the app will do so for you:
 # shiny, shinydashboard, tidyverse, DT, minpack.lm, broom, modelr
-#
-# then run these two lines
+
+
+# Just run these two lines
+# -> You need to install the shiny package first with install.packages("shiny")
 
 library(shiny)
 runGitHub("ShinyFlow", "jannikbuhr")
